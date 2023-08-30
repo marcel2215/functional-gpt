ff8bad386d9e5558ae948b68dcceb7578184e075
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-08-30 22:23:21 +0200

    Add methods for quick Message adding

--- src/Conversation.cs
+++ src/Conversation.cs
@@ -3,4 +3,34 @@
 public record Conversation
 {
     public List<Message> Messages { get; set; } = new();
+
+    public void FromSystem(string message)
+    {
+        Messages.Add(Message.FromSystem(message));
+    }
+
+    public void FromUser(string message)
+    {
+        Messages.Add(Message.FromUser(message));
+    }
+
+    public void FromUser(string name, string message)
+    {
+        Messages.Add(Message.FromUser(name, message));
+    }
+
+    public void FromAssistant(string message)
+    {
+        Messages.Add(Message.FromAssistant(message));
+    }
+
+    public void FromAssistant(FunctionCall functionCall)
+    {
+        Messages.Add(Message.FromAssistant(functionCall));
+    }
+
+    public void FromFunction(string name, string message)
+    {
+        Messages.Add(Message.FromFunction(name, message));
+    }
 }
