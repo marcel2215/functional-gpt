0ce2da6194078f496043bb0e8eb7bf3844679d33
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-08-30 21:38:54 +0200

    Add Message record

---
+++ src/Message.cs
@@ -0,0 +1,31 @@
+﻿namespace FunctionalGPT;
+
+public record Message
+{
+    public Message(Role role, string content)
+    {
+        Role = role;
+        Content = content;
+    }
+
+    public Message(Role role, string name, string content)
+    {
+        Role = role;
+        Name = name;
+        Content = content;
+    }
+
+    public Message(FunctionCall functionCall)
+    {
+        Role = Role.Assistant;
+        FunctionCall = functionCall;
+    }
+
+    public Role Role { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Content { get; set; }
+
+    public FunctionCall? FunctionCall { get; set; }
+}
