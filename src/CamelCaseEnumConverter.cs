51a72da046c9a9a2c122586cc8f15a72d50d96fa
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-08-30 22:35:06 +0200

    Add CamelCaseEnumConverter class

---
+++ src/CamelCaseEnumConverter.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FunctionalGPT;
+
+internal class CamelCaseEnumConverter : JsonStringEnumConverter
+{
+    internal CamelCaseEnumConverter() : base(JsonNamingPolicy.CamelCase) { }
+}
