7b63b311e79165e64a5c0c8c66d708ce45b9cb9c
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-08-30 21:29:40 +0200

    Add FunctionCall record

---
+++ src/FunctionCall.cs
@@ -0,0 +1,14 @@
+﻿namespace FunctionalGPT;
+
+public record FunctionCall
+{
+    public FunctionCall(string name, string arguments)
+    {
+        Name = name;
+        Arguments = arguments;
+    }
+
+    public string Name { get; set; }
+
+    public string Arguments { get; set; }
+}
