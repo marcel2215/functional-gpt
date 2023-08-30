8a43e5bfaa0c30ac29c58262cb7630e82313d0dd
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-08-30 22:04:57 +0200

    Add CompletionRequest record

---
+++ src/CompletionRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace FunctionalGPT;
+
+internal record CompletionRequest
+{
+    public CompletionRequest(string model, string messages)
+    {
+        Model = model;
+        Messages = messages;
+    }
+
+    public string Model { get; set; }
+
+    public string Messages { get; set; }
+}
