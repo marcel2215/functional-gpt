4122bfcd33c37df8060d60cb89af7b65cc6c131a
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-09-01 00:06:53 +0200

    Add StringCaseConverter class

---
+++ src/StringCaseConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace FunctionalGPT;
+
+internal static class StringCaseConverter
+{
+    internal static string ToSnakeCase(this string value)
+    {
+        var result = new StringBuilder();
+        for (var i = 0; i < value.Length; i++)
+        {
+            var c = value[i];
+            if (char.IsUpper(c))
+            {
+                if (i > 0 && value[i - 1] != '_')
+                {
+                    _ = result.Append('_');
+                }
+
+                _ = result.Append(char.ToLower(c));
+            }
+            else
+            {
+                _ = result.Append(c);
+            }
+        }
+
+        return result.ToString();
+    }
+}
